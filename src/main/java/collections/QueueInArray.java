675467ee3afd68252c3c328205a8926312cc3ad1
Maria101 <maria.bilous101@gmail.com>
2019-08-20 11:58:02 +0300

    javacore: Queue as an Array

---
+++ src/main/java/collections/QueueInArray.java
@@ -0,0 +1,57 @@
+package collections;
+
+public class QueueInArray { //a queue based upon an array - example made with chars & Alphabet
+    char[] charQ;
+    int putIndex;
+    int getIndex;
+
+    QueueInArray(int size) {
+        charQ = new char[size + 1];
+        putIndex = 0;
+        getIndex = 0;
+    }
+
+    void put(char ch) {
+        if (putIndex == charQ.length - 1) {
+            System.out.println(" - the queue is full");
+            return;
+        }
+        putIndex++;
+        charQ[putIndex] = ch;
+    }
+
+    char get() {
+        if (getIndex == putIndex) {
+            System.out.println(" - the queue is empty");
+            return (char) 0;
+        }
+        getIndex++;
+        return charQ[getIndex];
+    }
+
+    public static void main(String[] args) {
+        QueueInArray bigQueue = new QueueInArray(50);
+        QueueInArray smallQueue = new QueueInArray(3);
+
+        for (int i = 0; i < 26; i++) { // example with a sufficient size array
+            bigQueue.put((char) ('A' + i));
+        }
+        System.out.print("Big queue contains: ");
+        for (int i = 0; i < 26; i++) {
+            char chr = bigQueue.get();
+            System.out.print(chr);
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 4; i++) { // example with an insufficient size array
+            System.out.println("Placing " + (char) ('Z' - i) + " in the small queue");
+            smallQueue.put((char) ('Z' - i));
+        }
+        System.out.print("Small queue contains: ");
+        for (int i = 0; i < 4; i++) {
+            char chr = smallQueue.get();
+            if (chr != 0) System.out.print(chr);
+        }
+    }
+}

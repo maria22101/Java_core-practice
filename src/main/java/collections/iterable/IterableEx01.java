4a7d9c15df2bbd5f6d2ee9578c8062bdf5e28ce1
Maria101 <maria.bilous101@gmail.com>
2019-08-22 00:31:58 +0300

    javacore: Iterable implementation example

---
+++ src/main/java/collections/iterable/IterableEx01.java
@@ -0,0 +1,23 @@
+package collections.iterable;
+
+import java.util.Iterator;
+
+public class IterableEx01 implements Iterable<String>{
+    String[] it = {"first", "second", "third", " fourth"};
+
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+            int counter = 0;
+            @Override
+            public boolean hasNext() {
+                return counter < it.length;
+            }
+
+            @Override
+            public String next() {
+                return it[counter++];
+            }
+        };
+    }
+}

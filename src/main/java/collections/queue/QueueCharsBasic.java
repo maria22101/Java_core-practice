bb067633b87ffff281c4b05b26f1ec6a06eeeb1d
Maria101 <maria.bilous101@gmail.com>
2019-09-22 14:33:31 +0300

    javacore: Queue of chars variations - Interface example

---
+++ src/main/java/collections/queue/QueueCharsBasic.java
@@ -0,0 +1,6 @@
+package collections.queue;
+
+public interface QueueCharsBasic {
+    void put(char ch);
+    char get();
+}
---
+++ src/main/java/collections/queue/QueueChartsFixed.java
@@ -0,0 +1,28 @@
+package collections.queue;
+
+public class QueueChartsFixed implements QueueCharsBasic {
+    private char[] q;
+    private int putIndex, getIndex;
+
+    public QueueChartsFixed(int size){
+        q = new char[size + 1];
+        putIndex = getIndex = 0;
+    }
+
+    @Override
+    public void put(char ch) {
+        if (putIndex == q.length - 1){
+            System.out.println("Queue is full.");
+            return;
+        }
+        q[putIndex++] = ch;
+    }
+
+    @Override
+    public char get() {
+        if (getIndex == putIndex){
+            System.out.println("Queue is empty");
+        }
+        return q[getIndex++];
+    }
+}
